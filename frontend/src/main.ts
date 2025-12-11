c900dd0d5dbbe847e947b1d428c68b0f4c8b55da
Raul Garcia <raulgarciacanet@gmail.com>
2025-12-11 11:32:27 +0100

    - Components ProductsLists and ProductCard - Add TanStack query

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 import App from './App.vue';
 import router from './router';
@@ -10,5 +11,8 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueQueryPlugin, {
+  enableDevtoolsV6Plugin: true,
+});
 
 app.mount('#app');
---
+++ frontend/src/modules/products/interfaces/product.interface.ts
@@ -0,0 +1,25 @@
+import type { User } from '@/modules/auth/interfaces/user.interface';
+
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  slug: string;
+  stock: number;
+  sizes: Size[];
+  gender: string;
+  tags: string[];
+  images: string[];
+  user: User;
+}
+
+export enum Size {
+  l = 'L',
+  m = 'M',
+  s = 'S',
+  xl = 'XL',
+  xs = 'XS',
+  xxl = 'XXL',
+  xxxl = 'XXXL',
+}
