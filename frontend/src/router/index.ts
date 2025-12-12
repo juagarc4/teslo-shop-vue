725ad9f0735a7eaeb5c92ee7fdff41b5fe9dc27e
Raul Garcia <raulgarciacanet@gmail.com>
2025-12-12 16:24:03 +0100

    - Add login/register layouts - Add login action with axions and error handling - Add login/register views

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
-import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
+import { authRoutes } from '@/modules/auth/routes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,14 +17,8 @@ const router = createRouter({
         },
       ],
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    // AuthRoutes
+    authRoutes,
   ],
 });
 
